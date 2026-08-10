8be7b2d0738dc8467358c860b3d1c32c95f66317
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-09 23:29:09 -0300

    variaveis

---
+++ javascript/variaveis.js
@@ -0,0 +1,35 @@
+// Pensar em um nome para a variável
+
+// Operador de atribuição =
+
+// variáveis não podem começar com números, não podem ter espaços, não podem ter caracteres especiais, não podem ser palavras reservadas
+
+// variável global
+
+idade = 35;
+
+console.log('Minha idade atual:', idade);
+
+dataAniversario = '24/04/1991';
+
+console.log('Data de aniversário: ', dataAniversario);
+
+idade = 36;
+
+console.log('Minha idade depois do aniversário: ', idade);
+
+// As diferentes formas de declarar variáveis são: var, let e const
+
+var nome = 'João';
+
+console.log('Meu nome é: ', nome);
+
+let sobrenome = 'Silva';
+
+console.log('Meu sobrenome é: ', sobrenome);
+
+// const é uma variável que não pode ser alterada, ou seja, é uma constante
+const cidade = 'São Paulo';
+
+console.log('Minha cidade é: ', cidade);
+
