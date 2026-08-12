791a30eff7468d1e445a83c6cf7f0f03ee8859fd
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-11 21:03:41 -0300

    operadores de comparacao

---
+++ javascript/operadores-de-comparacao.js
@@ -0,0 +1,26 @@
+// Operadores de comparação
+// >, <, >=, <=, ==, ===, !=, !==
+
+// Maior que
+console.log(10 > 5); // true
+
+// Menor que
+console.log(10 < 5); // false
+
+// Maior ou igual a
+console.log(10 >= 10); // true
+
+// Menor ou igual a
+console.log(10 <= 5); // false
+
+// Igual a (valor)
+console.log(10 == '10'); // true
+
+// Igual a (valor e tipo)
+console.log(10 === '10'); // false
+
+// Diferente de (valor)
+console.log(10 != '10'); // false
+
+// Diferente de (valor e tipo)
+console.log(10 !== '10'); // true
